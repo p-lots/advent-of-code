f1bebc4b6fc262a25cac8fad6f5d547bd5c18051
Paul Calotta <paul.calotta@gmail.com>
2019-05-29 11:22:50 -0400

    Added README, some Swift solutions

---
+++ 2015/day4/part2.swift
@@ -0,0 +1,38 @@
+import Foundation
+import CommonCrypto
+
+// begin code taken from https://stackoverflow.com/questions/32163848/how-can-i-convert-a-string-to-an-md5-hash-in-ios-using-swift
+
+func MD5(_ string: String) -> String? {
+    let length = Int(CC_MD5_DIGEST_LENGTH)
+    var digest = [UInt8](repeating: 0, count: length)
+    
+    if let d = string.data(using: String.Encoding.utf8) {
+        _ = d.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
+            CC_MD5(body, CC_LONG(d.count), &digest)
+        }
+    }
+    
+    return (0..<length).reduce("") {
+        $0 + String(format: "%02x", digest[$1])
+    }
+}
+
+// end code taken from stackoverflow
+
+func part2(_ input: String) -> Int {
+    var ret = 0
+    while true {
+        let testString: String = "\(input)\(ret)"
+        if let md5String = MD5(testString) {
+            let sixthIndex = md5String.index(md5String.startIndex, offsetBy: 5)
+            if md5String[md5String.startIndex...sixthIndex] == "000000" {
+                break
+            }
+        }
+        ret += 1
+    }
+    return ret
+}
+
+print(part2("yzbqklnj"))
--- 2018/day1/part1.swift
+++ 2018/day1/part1.swift
@@ -5,23 +5,3 @@ func partOne(_ input: [String]) -> Int {
 }
 
 print(partOne(input))
-
-func partTwo(_ input: [String]) {
-	let inputFixed = input.map { Int(String($0)) ?? 0 }
-	var alreadySeen = Set<Int>()
-	var freq = 0
-	var foundRepeat = false
-	while !foundRepeat {
-		for val in inputFixed {
-			freq += val
-			let (insertWasSuccessful, insertedValue) = alreadySeen.insert(freq)
-			if !insertWasSuccessful {
-				print(insertedValue)
-				foundRepeat = !insertWasSuccessful
-				break
-			}
-		}
-	}
-}
-
-partTwo(input)
