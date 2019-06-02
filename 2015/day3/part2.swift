cb65c24e0443803a08e6cd6ce3bff88eada5004d
Paul Calotta <paul.calotta@gmail.com>
2019-06-02 14:04:44 -0400

    Added README for 2016/day1 and part 2 Swift solution for 2015/day3

---
+++ 2015/day3/part2.swift
@@ -0,0 +1,56 @@
+func part2(_ input: String) -> Int {
+    var xCount = 1, yCount = 1
+    for dir in input {
+        switch dir {
+            case "<":
+            fallthrough
+            case ">":
+            xCount += 1
+            case "^":
+            fallthrough
+            case "v":
+            yCount += 1
+            default: break
+        }
+    }
+    var grid = (0..<xCount).map { _ in return Array<Int>(repeating: 0, count: yCount) }
+    var currX = (grid[0].count % 2 == 0 ? grid[0].count / 2 + 1 : grid[0].count / 2)
+    var currY = (grid.count % 2 == 0 ? grid.count / 2 + 1 : grid.count / 2)
+    var currRoboX = currX, currRoboY = currY
+    grid[currY][currX] = 1
+    for (i, dir) in input.enumerated() {
+        if i % 2 == 0 {
+            switch dir {
+                case "^":
+                currY += 1
+                case "v":
+                currY -= 1
+                case "<":
+                currX += 1
+                case ">":
+                currX -= 1
+                default: break
+            }
+            grid[currY][currX] += 1
+        } else {
+            switch dir {
+                case "^":
+                currRoboY += 1
+                case "v":
+                currRoboY -= 1
+                case "<":
+                currRoboX += 1
+                case ">":
+                currRoboX -= 1
+                default: break
+            }
+            grid[currRoboY][currRoboX] += 1
+        }
+    }
+    let ret = grid.reduce(0) { (acc: Int, row: [Int]) -> Int in
+        return acc + row.reduce(0) { (innerAcc: Int, house: Int) -> Int in
+            return innerAcc + (house > 0 ? 1 : 0)
+        }
+    }
+    return ret
+}
