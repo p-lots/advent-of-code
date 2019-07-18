4576101cfc14c5dbcf6cd74e5c0cebcbc870ef25
Paul Calotta <paul.calotta@gmail.com>
2019-07-18 18:16:01 -0400

    Added Swift solutions for 2017/day5

---
+++ 2017/day5/part1.swift
@@ -0,0 +1,16 @@
+aimport Foundation
+
+func part1(_ input: String) -> Int {
+    var inputSplit = input.components(separatedBy: "\n").compactMap { Int(String($0)) }
+    var currPos = 0, jumps = 0
+    while true {
+        jumps += 1
+        let jumpDist = inputSplit[currPos]
+        if currPos + jumpDist >= inputSplit.count {
+            break
+        }
+        inputSplit[currPos] += 1
+        currPos += jumpDist
+    }
+    return jumps
+}
